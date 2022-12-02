b46843479f9d203daafebf29682ea58cfcc7c5c2
unknown <mateus@tminformatica.com.br>
2022-12-02 15:49:50 -0300

    Ajsute

--- index.js
+++ index.js
@@ -6,28 +6,12 @@ const app = express();
 
 const db = require("./db")
 
-// corsOptions = {
-//     origin: '*'
-// }
+corsOptions = {
+    origin: '*'
+}
 
-// app.use(cors(corsOptions))
+app.use(cors(corsOptions))
 // fim cors
-
-//configurando express
-app.use((req, res, next) => {
-    express.urlencoded({
-        extended: true
-    })
-
-    res.header("Access-Control-Allow-Origin", "*");
-
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-
-    app.use(cors());
-    
-    next();
-});
-
 
 app.use(express.json())
 //express
