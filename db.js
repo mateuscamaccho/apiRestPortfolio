7490e1dd741b3164cd8d4e42a48ab7b4b7d88300
unknown <mateus@tminformatica.com.br>
2022-12-02 14:58:47 -0300

    build

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ async function connect() {
         return global.connection;
 
     const mysql = require("mysql2/promise");
-    const connection = await mysql.createPool("mysql://gssensormain:Ovridapa032!@187.45.196.194/gssensormain");
+    const connection = await mysql.createPool("mysql://gssensormain:Ovridapa032!@187.45.196.194/gssensormain"); 
     console.log("Conectou no MySQL!");
     global.connection = connection;
     return connection;
