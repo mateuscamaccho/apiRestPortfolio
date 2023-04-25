32b49bc99f9ee50c992cacaddb739fe0aa751edc
unknown <mateus@tminformatica.com.br>
2023-04-25 11:16:43 -0300

    ajuste para usar env variables

--- api/db.js
+++ api/db.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 async function connect() {
     if (global.connection && global.connection.state !== 'disconnected')
         return global.connection;
 
     const mysql = require("mysql2/promise");
-    const connection = await mysql.createPool("mysql://gssensormain:Ovridapa032!@187.45.196.194/gssensormain"); 
+    const connection = await mysql.createPool(process.env.DATA_URL); 
     console.log("Conectou no MySQL!");
     global.connection = connection;
     return connection;
